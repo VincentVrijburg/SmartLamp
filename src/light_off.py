2403db75174bd24743bd7d06a27a89c01724e997
VincentVrijburg <vincentvrijburg@gmail.com>
2019-09-18 11:57:53 +0200

    Add project files

---
+++ src/light_off.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import relay as r
+
+# Set the output for the relay module.
+pin_relay_out = 7
+
+# Set the correct RPi.GPIO mode.
+GPIO.setmode(GPIO.BOARD)
+
+# Initialize the relay class.
+relay = r.Relay(pin_relay_out)
+
+# Turn the relay on in order to turn the light on.
+relay.turn_off()
---
+++ src/main.py
@@ -0,0 +1,66 @@
+# Import necessary modules.
+import RPi.GPIO as GPIO
+from datetime import datetime, timedelta
+import relay as r
+import motion_sensor as ms
+import light_sensor as ls
+
+# Set the different inputs/outputs for the Raspberry Pi modules and sensors.
+pin_motion_sensor_in = 11
+pin_light_sensor_in = 12
+pin_relay_out = 7
+
+# Set the correct RPi.GPIO mode.
+GPIO.setmode(GPIO.BOARD)
+
+# Initialize the different classes.
+motionSensor = ms.MotionSensor(pin_motion_sensor_in)
+lightSensor = ls.LightSensor(pin_light_sensor_in)
+relay = r.Relay(pin_relay_out)
+
+while 1:
+    if ((relay.lastTimeTurnedOn is not None) and ((relay.lastTimeTurnedOn + timedelta(seconds=5) > datetime.now()))):
+        if (motionSensor.motionDetected):
+            print("Het licht blijft aan, en er is beweging gedetecteerd")
+            relay.lastTimeTurnedOn = datetime.now()
+            relay.turn_on()
+        else:
+            print("Het licht blijft aan")
+            relay.turn_on()
+    else:
+        if (motionSensor.motionDetected):
+            needsLight = lightSensor.is_dark_environment()
+            if (needsLight == True):
+                print("Beweging gedetecteerd, het licht zal aangezet worden indien nodig")
+                relay.lastTimeTurnedOn = datetime.now()
+                relay.turn_on()
+            else:
+                print("Beweging gedetecteerd, maar het licht is niet nodig")
+                relay.turn_off()
+        else:
+            relay.turn_off()
+
+# try:
+#     while 1:
+#         if ((relay.lastTimeTurnedOn is not None) and ((relay.lastTimeTurnedOn + timedelta(seconds=5) > datetime.now()))):
+#             relay.turn_on()
+#         else:
+#             if (motionSensor.motionDetected):
+#                 needsLight = lightSensor.is_dark_environment()
+#                 if (needsLight == True):
+#                     relay.lastTimeTurnedOn = datetime.now()
+#                     relay.turn_on()
+#                 else:
+#                     relay.turn_off()
+#             else:
+#                 relay.turn_off()
+# except KeyboardInterrupt:
+#     print("Keyboard interrupt")
+# except:
+#     print("some error") 
+# finally:
+#     print("clean up") 
+#     # Dispose the dependencies
+#     motionSensor.dispose()
+#     lightSensor.dispose()
+#     relay.dispose()
